5c618e138979f322cf4e77ac0168bf1aee3c913b
sin <sin@2f30.org>
2014-07-22 14:19:52 +0100

    It is -Wno-clobbered not -Wno-clobber

--- config.mk
+++ config.mk
@@ -7,7 +7,7 @@ PREFIX = /usr/local
 LIBS = -lpng
 
 # flags
-CFLAGS = -std=c90 -pedantic -ansi -Wall -Wextra -Wno-pointer-sign -Wno-maybe-uninitialized -Wno-clobber -Os
+CFLAGS = -std=c90 -pedantic -ansi -Wall -Wextra -Wno-pointer-sign -Wno-maybe-uninitialized -Wno-clobbered -Os
 LDFLAGS = -s ${LIBS}
 
 # compiler and linker
