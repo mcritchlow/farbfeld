9f7647d61eb07195fc46cd4d9ad73aab06f2f779
sin <sin@2f30.org>
2014-07-29 17:25:22 +0100

    Revert "Fix endianness in header"
    The previous behaviour was correct.  A width of 1900 pixels which is
    0x00000640 in hex was stored on disk as:
    
    00 00 06 40
    
    That's BE order which is what we want.
    
    Reading this uint32_t on an LE machine we get:
    
    width = 0x40060000 and converting from BE to LE
    
    we get the correct 0x640 value.
    
    Reading on a BE machine we get the correct value
    and the swap is a no-op.

--- png2if.c
+++ png2if.c
@@ -57,8 +57,10 @@ main(int argc, char *argv[])
 
 	/* write header with big endian width and height-values */
 	fprintf(stdout, "imagefile");
-	fwrite(&width, sizeof(uint32_t), 1, stdout);
-	fwrite(&height, sizeof(uint32_t), 1, stdout);
+	val_be = htonl(width);
+	fwrite(&val_be, sizeof(uint32_t), 1, stdout);
+	val_be = htonl(height);
+	fwrite(&val_be, sizeof(uint32_t), 1, stdout);
 
 	/* write data */
 	for (i = 0; i < height; i++) {
