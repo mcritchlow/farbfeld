86495f8058518f7064f2ddda92314aa42163b4c0
FRIGN <dev@frign.de>
2016-01-04 23:11:25 +0100

    Update config.mk
    -Wno-clobbered is not supported on OpenBSD. Let's remove it and Wextra
    with all its stupid warnings.

--- config.mk
+++ config.mk
@@ -6,12 +6,15 @@ PREFIX = /usr/local
 PNGLIB = /usr/local/lib
 PNGINC = /usr/local/include
 
-INCS = -I${PNGINC}
-LIBS = -L${PNGLIB} -lpng -ljpeg
+JPEGLIB = /usr/local/lib
+JPEGINC = /usr/local/include
+
+INCS = -I${PNGINC} -I${JPEGINC}
+LIBS = -L${PNGLIB} -L${JPEGLIB} -lpng -ljpeg
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE
-CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Wno-clobbered -Os ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = -s ${LIBS}
 
 # compiler and linker
